fc3499b62d942c3fc13629ecdfe317ec2ca37906
Karthik <karthik.krishnaswamy@ratnaglobaltech.com>
2024-03-08 13:12:29 +0530

    Update src/App.js

--- src/App.js
+++ src/App.js
@@ -1,28 +1,35 @@
-import React, { useEffect, useState } from 'react';
+// frontend/src/App.js
+import React, { useState, useEffect } from 'react';
+import './App.css';
 
 function App() {
-  const [data, setData] = useState(null);
+  const [backendResponse, setBackendResponse] = useState('');
+  const [timestamp, setTimestamp] = useState('');
+  
 
   useEffect(() => {
+    // Fetch data from backend API
     const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/data';
-
     fetch(apiUrl)
       .then(response => response.json())
-      .then(data => setData(data))
-      .catch(error => console.error('Error fetching data:', error));
+      .then(data => {
+        setBackendResponse(data.message);
+        setTimestamp(data.timestamp)
+      })
+      .catch(error => {
+        console.error('Error fetching data:', error);
+      });
   }, []);
 
   return (
-    <div>
-      {data && (
-        <div>
-          <p>{data.message}</p>
-          {/* <p>{data.timestamp}</p> */}
-        </div>
-      )}
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to My Frontend App</h1>
+        <p>{backendResponse}</p>
+        <p>Timestamp: {timestamp}</p> {/* Display the timestamp */}
+      </header>
     </div>
   );
 }
-
 
 export default App;
