821d632e7b58c315dab8f2ff4d122b0540b44684
Karthik <karthik.krishnaswamy@ratnaglobaltech.com>
2024-03-08 12:38:52 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    const apiUrl = process.env.REACT_APP_API_URL || 'http://localhost:5000/api/data';
+
+    fetch(apiUrl)
+      .then(response => response.json())
+      .then(data => setData(data))
+      .catch(error => console.error('Error fetching data:', error));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {data && (
+        <div>
+          <p>{data.message}</p>
+          {/* <p>{data.timestamp}</p> */}
+        </div>
+      )}
     </div>
   );
 }
+
 
 export default App;
